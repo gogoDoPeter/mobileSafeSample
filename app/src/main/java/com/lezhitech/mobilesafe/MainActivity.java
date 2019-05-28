a4a14ee8f42c1cf9815c47ab6f7a24f8d8dd5aee
actions <liuzhen1573@163.com>
2019-05-28 14:40:34 +0800

    add button for test

--- app/src/main/java/com/lezhitech/mobilesafe/MainActivity.java
+++ app/src/main/java/com/lezhitech/mobilesafe/MainActivity.java
@@ -2,6 +2,8 @@ package com.lezhitech.mobilesafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void test(View view) {
+        Toast.makeText(getApplicationContext(),"this is a test",Toast.LENGTH_LONG).show();
     }
 }
